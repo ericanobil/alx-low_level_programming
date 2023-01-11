30fc77944e7264c19971c81a844da7306eab221f
Nana Kweku Anobil <78821795+ericanobil@users.noreply.github.com>
2023-01-11 15:25:18 +0200

    101-natural.c
    A program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,7 @@
 
 /**
  * main - main body of the code block
- * Description:-this programme prints the sum of all multiples of 3 or 5 below 1024.
+ * Description: this prgm prints the sum of all multiples of 3 or 5 below 1024.
  * Return:- 0
  */
 
