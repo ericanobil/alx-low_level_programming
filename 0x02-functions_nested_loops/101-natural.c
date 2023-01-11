98c11f9aa216622c056c60c654ed72216d38b2e9
Nana Kweku Anobil <78821795+ericanobil@users.noreply.github.com>
2023-01-11 12:13:24 +0200

    A program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,5 +2,32 @@
  #include <stdio.h>
  #include <math.h>
 
+/**
 
+* main :-  the main body of the ode block
+* Description :- Produces the sum of all the multiples of 3 or 5 below 1024
+* Return :- 0
+
+*/
+
+
+int main(void)
+{
+         int i = 1;
+         int total = 0;
+ 
+         while (i < 1024)
+         {
+                  if (i % 3 == 0)
+                          total += i;
+                  else if (i % 5 == 0)
+                           total += i;
+          
+                   i++;
+         }
+         printf("%d\n", total);
+ 
+         return (0);
+}
+                           
 
