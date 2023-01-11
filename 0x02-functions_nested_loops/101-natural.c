7366d3bae2226e206e76c73530a122410788082a
Nana Kweku Anobil <78821795+ericanobil@users.noreply.github.com>
2023-01-11 12:30:24 +0200

    101-natural.c
    A program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,20 +8,19 @@
 * Return : 0
 */
 
-
 int main(void)
 {
          int i = 1;
          int total = 0;
- 
+
          while (i < 1024)
          {
                   if (i % 3 == 0)
                           total += i;
                   else if (i % 5 == 0)
                            total += i;
-          
-                   i++;
+                           
+                  i++;
          }
          printf("%d\n", total);
  
