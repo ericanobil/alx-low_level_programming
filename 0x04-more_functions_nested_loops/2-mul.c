720a30c05c15eaa1290c3c63767bf8b74712d05e
Nana Kweku Anobil <78821795+ericanobil@users.noreply.github.com>
2023-01-17 10:37:41 +0200

    2-mul.c
    A function that runs the multiples of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * mul - multiplies two digits
+  * @x: first digit
+  * @y: second digit
+  *
+  *Return: 1 if it's succesfully run
+  */
+int mul(int x, int y)
+{
+	int z;
+
+	z = x * y;
+	return (z);
+}
