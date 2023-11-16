65ab6c31cf3a2a5fc984f20ed6a5467ae6e3a30f
ericanobil <e.anobillel@gmail.com>
2023-11-16 01:40:48 -0800

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
 
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -12,4 +15,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
+#endif/*MAIN_H*/
